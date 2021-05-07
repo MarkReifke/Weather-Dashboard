3eff728f7978b72595186ac5a8b121827ead448f
Mark Reifke <mark.reifke@gmail.com>
2021-05-07 10:10:30 -0700

    chaged coding

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,103 +1,124 @@
-var api_key = "1b5491b3e5b0b634e1e69f676a621a18";
-var reportEl = document.querySelector('.report');
-var cardEl = document.querySelector('.card');
-var tempEl = document.querySelector('.temperature')
-var humidityEl = document.querySelector(".humidity")
-var windEl = document.querySelector(".wind")
-var uvEl = document.querySelector(".uv")
-var cityEl = document.querySelector('.city')
-var dayEl = document.querySelector('.day')
-var savedEl = document.querySelector('.saved')
-var forcastEl = document.querySelector('.forcast')
+const api_key = "1b5491b3e5b0b634e1e69f676a621a18";
+var cityName;
+var searchHistory = JSON.parse(localStorage.getItem('history')) || [];
+var currentTemp;
+var currentIcon;
+var currentHumidity;
+var currentWind;
+var currentUVI;
+var forecastTemp = [];
+var forecastHumidity = [];
+var forecastIcon= [];
 
-var today = moment().format('MMMM Do YYYY');
+function getWeather(city) {
+    cityName = city;
+    const currentWeatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${api_key}`;
+    fetch(currentWeatherUrl)
+        .then((data) => data.json())
+        .then(function (weather) {
+            if (weather.cod === "404") {
+                alert("City not found");
+                return;
+            }
+            var lat = weather.coord.lat;
+            var lon = weather.coord.lon;
+            const oneCallUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${api_key}&units=imperial`;
+            fetch(oneCallUrl)
+                .then((data) => data.json())
+                .then(function (oneCallData) {
+                    
 
-var savedCities = JSON.parse(localStorage.getItem("recent")) || [];
+                    
+                    currentTemp = oneCallData.current.temp
+                    currentHumidity = oneCallData.current.humidity
+                    currentWind = oneCallData.current.wind_speed
+                    currentUVI = oneCallData.current.uvi
+                    currentIcon = oneCallData.current.weather[0].icon
+                   
+                    
 
-var city = '';
-function retreiveWeather(city) {
-    var currentURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apikey}`;
-    fetch(currentURL)
-       .then((data) => data.json())
-       .then(function (weather) { 
-      if (weather.cod === "404") {
-      alert("Not found");
-     };    
-    var lat = weather.coord.lat;
-    var lon = weather.coord.lon;
-    var onecallURL = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=imperial&appid=${apikey}`;        
-     fetch(onecallURL)
-    .then((data) => data.json())
-    .then(function (oneCallData) {
-     console.log(oneCallData)                 
-     var report = oneCallData.current
-     $(reportEl).show();      
-    cityEl.innerHTML = "  ";
-      cityEl.append(city);   
-     dayEl.textContent = today;
-var temperature = `Temperature: ${report.temp} °F`;
-    tempEl.textContent = temperature;
-   var humidity = `Humidity: ${report.humidity} %`;
-     humidityEl.textContent = humidity;
-    var wind = `Wind Speed: ${report.wind_speed} MPH`;
-    windEl.textContent = wind;
-   var uvindex = `${report.uvi}`;
- uvEl.textContent = uvindex;
-   if (report.uvi <= 2) {
-     uvEl.classList.add('low');
-  } else if (report.uvi > 2 && report.uvi < 6) {
-     uvEl.addClass("mid")
-   } else if (report.uvi > 5 && report.uvi < 8) {
-     uvEl.addClass("high")
-   } else if (report.uvi > 7 && report.uvi < 11) {
-     uvEl.addClass("vhigh")
-   } else {
-     uvEl.addClass("extreme")
-    };
-  var forcastData = oneCallData.daily;                
-    forcastData.shift();
-   forcastData.pop();
-    forcastData.pop();
-     console.log(forcastData);
-     console.log(forcastData[0].clouds);
-     for (let i = 0; i < forcastData.length; i++) {
-     var forcastCard = $(`
-       <div class="card">
-       <h1>Date<h1>
-       <p>Temp: ${forcastData[i].temp.max}°F </p>
-       <p>Humidity: ${forcastData[i].humidity}%</p>
-       </div >
-            `)
-       forcastEl.append(forcastCard);
-       }
-      })
-   })
+                    
+                    forecastTemp = [];
+                    forecastHumidity = [];
+                    forecastIcon= [];
+                    for (let i = 0; i < 5; i++) {
+                        forecastTemp.push(oneCallData.daily[i].temp.day)
+                        forecastHumidity.push(oneCallData.daily[i].humidity)
+                        forecastIcon.push(oneCallData.daily[i].weather[0].icon)
+                       
+
+                    }
+                    
+                    if(!(searchHistory.includes(cityName))){
+                        searchHistory.push(cityName)
+                    }
+
+                    displayForeCast();
+                    displayCurrentInfo();
+                    displayHistory();
+                    localStorage.setItem('history', JSON.stringify(searchHistory));
+
+                });
+            });
+            ;
+            
 }
-function saveRecent(city) {
-    if (savedCities.includes(city)) {
-        return;
-    }
-    if (savedCities.length === 7) {
-        savedCities.pop();
-    }
-    savedCities.unshift(city);
-    localStorage.setItem("recent", JSON.stringify(savedCities));
-    pullRecent(savedCities);
+
+function displayCurrentInfo() {
+    var date = new Date();
+    var weatherDisplay = document.getElementById("currentweather");
+    weatherDisplay.innerHTML =
+ `<div class= "card-body">
+    <h2 class="card-title text-light">${cityName} <img class ="wIcons" src = "https://openweathermap.org/img/wn/${currentIcon}@2x.png"></img> ${date.toLocaleString('en-US', { month: "numeric", day: "numeric", year: "numeric" })}</h2>
+    <div class="card-text text-light">Temperature: ${currentTemp}&#176</div>
+    <div class= "card-text text-light">Humidity: ${currentHumidity}%        
+    <div class="card-text text-light">Wind-Speed: ${currentWind}</div>
+    <div class="card-text text-light">UVI: ${currentUVI}</div></div>`
+
 }
-function pullRecent(arr) {
-    savedEl.innerHTML = "";
-    for (let i = 0; i < arr.length; i++) {
-        var item = arr[i];
-        var li = document.createElement("li");
-        li.innerText = item;
-        savedEl.append(li);
+
+function displayForeCast() {
+    var date = new Date();
+    var fiveDay = document.getElementById("fiveday");
+    fiveDay.innerHTML = "";
+    
+    
+    for (let i = 0; i < 5; i++) {
+        date.setDate(date.getDate() + 1);
+        fiveDay.innerHTML += `
+        <div class="card fCard" >
+        <h6 class="card-title">${date.toLocaleString('en-US', { month: "numeric", day: "numeric", year: "numeric" })}</h6>
+        <img class ="wIcons" src= https://openweathermap.org/img/wn/${forecastIcon[i]}@2x.png ><img>
+        <div class="card-text">Temperature: ${forecastTemp[i]}</div><div class="card-text">Humidity: ${forecastHumidity[i]}</div>
+        </div>`
+        
     }
 }
-pullRecent(savedCities)
-$("#search").on('click', function (event) {
-    event.preventDefault();
-    var citySearch = $(this).siblings("input").val();
-    console.log(citySearch);
-    retreiveWeather(citySearch);
-    saveRecent(citySearch);
-});
+
+function displayHistory() {
+    var pastDisplay = document.getElementById("pastsearch");
+    pastDisplay.innerHTML = "";
+    ;
+    
+     for (let i = 0; i < searchHistory.length; i++) {
+       pastDisplay.innerHTML += `<li class="searchlist">${searchHistory[i]}</li>`;
+    }
+}
+
+function init() {
+   
+        displayHistory();
+    
+
+    document.getElementById("searchForm").addEventListener("submit", function (e) {
+        e.preventDefault();
+        var searchCity = document.getElementById("userInput").value;
+        getWeather(searchCity);
+    })
+    document.getElementById("pastsearch").addEventListener("click", function (e) {
+        e.preventDefault();
+        getWeather(e.target.textContent);
+    })
+}
+
+init();
